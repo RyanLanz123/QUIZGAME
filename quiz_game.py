187fc05ea021bc17c0ca706ae6094d3746306249
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-07 11:39:30 +0000

    add basic if/else & inputs

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,14 @@
 print("Welcome to my computer quiz")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay, let's play:")
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect.")
